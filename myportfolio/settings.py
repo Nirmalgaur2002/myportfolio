230581a48c3c8b8c193708862121ae19debb4c16
Naveen-gaur15 <gaur.nk15@gmail.com>
2025-09-24 08:01:39 +0530

    Add Render domain to ALLOWED_HOSTS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'my_web', 'static')]
